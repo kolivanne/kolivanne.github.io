790693487e5297066678aebc255c63b17638b8b9
anne bahnemann <anne.bahnemann@gmx.de>
2026-04-04 23:01:31 +0200

    Added data

--- resources/js/data/cvData.js
+++ resources/js/data/cvData.js
@@ -1,8 +1,8 @@
 let cvData = [
   {
-    role: "Software Engineer",
-    company: "Medical Tech Company",
-    period: "2020 - 2023",
+    role: "Senior QA Automation Engineer / Software Engineer",
+    company: "Wooga",
+    period: "03/2023 - Today",
     highlights: [
       "Built End-to-End test automation frameworks",
       "Improved release reliability",
@@ -10,13 +10,33 @@ let cvData = [
     ]
   },
   {
-    role: "QA / Automation Engineer",
-    company: "Various Projects",
-    period: "2023 - Today",
+    role: "QA Engineer",
+    company: "DKB",
+    period: "05/2022 - 11/2022",
+    highlights: [
+      "Built End-to-End test automation frameworks",
+      "Improved release reliability",
+      "Integrated quality into development workflow"
+    ]
+  },
+  {
+    role: "Test Automation Engineer / Software Engineer",
+    company: "Qnit",
+    period: "09/2021 - 04/2022",
     highlights: [
       "Developed reusable C# testing modules",
       "Bridged development and testing processes",
       "Focused on system-level quality improvements"
+    ]
+  },
+  {
+    role: "Software Engineer",
+    company: "Dental Wings",
+    period: "05/2015 - 08/2021",
+    highlights: [
+      "Built End-to-End test automation frameworks",
+      "Improved release reliability",
+      "Integrated quality into development workflow"
     ]
   }
 ];
