9fcd04329457905b8a6f136cd43ac20be4fb184f
anne bahnemann <anne.bahnemann@gmx.de>
2026-06-02 16:31:51 +0200

    Updated data

--- resources/js/data/cvData.js
+++ resources/js/data/cvData.js
@@ -1,42 +1,44 @@
 let cvData = [
-  {
-    role: "Senior QA Automation Engineer / Software Engineer",
-    company: "Wooga",
-    period: "03/2023 - Today",
+   {
+    role: "Software Engineer | Embedded Systems, Automation & DevOps",
+    company: "Codialist GmbH",
+    period: "06/2026 - Today",
     highlights: [
-      "Built End-to-End test automation frameworks",
-      "Improved release reliability",
-      "Integrated quality into development workflow"
+    ]
+  },
+  {
+    role: "Senior Software Engineer | Automation, Tooling & DevOps",
+    company: "Wooga",
+    period: "03/2023 - 04/2026",
+    highlights: [
     ]
   },
   {
     role: "QA Engineer",
-    company: "DKB",
+    company: "DKB | Deutsche Kreditbank AG",
     period: "05/2022 - 11/2022",
     highlights: [
-      "Built End-to-End test automation frameworks",
-      "Improved release reliability",
-      "Integrated quality into development workflow"
     ]
   },
   {
-    role: "Test Automation Engineer / Software Engineer",
-    company: "Qnit",
+    role: "Software Engineer | Automation",
+    company: "Qnit AG",
     period: "09/2021 - 04/2022",
     highlights: [
-      "Developed reusable C# testing modules",
-      "Bridged development and testing processes",
-      "Focused on system-level quality improvements"
     ]
   },
   {
-    role: "Software Engineer",
+    role: "Software Engineer | Java , C# & Automation",
     company: "Dental Wings",
     period: "05/2015 - 08/2021",
     highlights: [
-      "Built End-to-End test automation frameworks",
-      "Improved release reliability",
-      "Integrated quality into development workflow"
+    ]
+  },
+    {
+    role: "Working Student | Quality Management",
+    company: "BIOTRONIK",
+    period: "07/2012 - 02/2015",
+    highlights: [
     ]
   }
 ];
